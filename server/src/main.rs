2ddb675bc80382f2661989a161db1ccf3ce3e4e4
skjaldr <strazku@gmail.com>
2023-12-21 19:59:50 -0600

    implemented new changes and error handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,118 +1,124 @@
 use std::{
-    thread,
-    io::{Read, Write},
-    net::{TcpStream, TcpListener}, sync::mpsc::{Receiver, Sender, self}, time::Duration,
+    net::{TcpStream, TcpListener},
+    sync::mpsc::{Sender, Receiver, self},
+    thread::{JoinHandle, self},
+    io::{Read, Write}
 };
 use random_string::generate;
 
-struct ClientMsg {
+fn main() {
+    let listener = match TcpListener::bind("0.0.0.0:7878") {
+        Ok(listener) => listener,
+        Err(e) => {
+            eprintln!("Failed to start server: {e}");
+            return;
+        }
+    };
+
+    let mut thread_vec: Vec<JoinHandle<_>> = Vec::new();
+    let (tx, rx) = mpsc::channel();
+
+    let broadcast = thread::spawn(move || {
+        broadcast(rx);
+    });
+
+    for stream in listener.incoming() {
+        let tx_clone = tx.clone();
+        match stream {
+            Ok(stream) => {
+                let client_id = gen_id();
+
+                let stream_clone = match stream.try_clone() {
+                    Ok(stream_clone) => stream_clone,
+                    Err(e) => {
+                        eprintln!("failed to clone stream: {e}");
+                        continue;
+                    }
+                };
+
+                if let Err(e) = tx_clone.send(ServerMessages::NewClient(stream_clone, client_id.clone())) {
+                    eprintln!("Failed to send client information over the channel: {e}");
+                    continue;
+                }
+
+                let handle = thread::spawn(move || {
+                    handle_clients(stream, tx_clone, client_id);
+                });
+                thread_vec.push(handle);
+            }
+            Err(e) => eprintln!("Failed to match stream: {e}"),
+
+        }
+    }
+}
+
+struct ClientMsgId {
     message: String,
     client_id: String,
 }
 
 enum ServerMessages {
-    Newclient(TcpStream, String),
-    ClientMessage(ClientMsg),
+    NewClient(TcpStream, String),
+    Messages(ClientMsgId),
 }
 
-fn broadcast_message(rx: Receiver<ServerMessages>) {
+fn handle_clients(mut stream: TcpStream, tx: Sender<ServerMessages>, client_id: String) {
+    loop {
+        let mut buf = [0; 4];
+        if let Ok(_) = stream.read_exact(&mut buf) {
+            let msg_size = u32::from_be_bytes(buf) as usize;
+            let mut msg_buf = vec![0; msg_size];
+
+            if let Ok(_) = stream.read_exact(&mut msg_buf) {
+                let message = String::from_utf8_lossy(&msg_buf).to_string();
+
+                if let Err(e) = tx.send(ServerMessages::Messages(
+                    ClientMsgId { message, client_id: client_id.clone() })) {
+                        eprintln!("Failed to send messages to clients: {e}");
+                        continue;
+                };
+            }
+        }
+    }
+}
+
+fn broadcast(rx: Receiver<ServerMessages>) {
     let mut clients: Vec<(TcpStream, String)> = Vec::new();
     loop {
-
-        // changed to using an Enum to utilize a single thread intead of two.
-        if let Ok(message) = rx.recv() {
-            match message {
-                ServerMessages::Newclient(new_client, id) => {
-                    clients.push((new_client, id));
+        while let Ok(new_client) = rx.recv() {
+            match new_client {
+                ServerMessages::NewClient(client, id) => {
+                    clients.push((client, id));
                 }
-                ServerMessages::ClientMessage(message) => {
+                ServerMessages::Messages(message) => {
+                    let message_buf = message.message.as_bytes();
+                    let msg_size = buf_size.len() as u32;
+                    let prefix_size = msg_size.to_be_bytes();
                     for (client, id) in clients.iter_mut() {
                         if message.client_id != *id {
-                            let _ = client.write_all(message.message.as_bytes()).unwrap();
+                            // broadcast dah messuge!
+
+                            if let Err(e) = client.write_all(&prefix_size) {
+                                eprintln!("Failed to write prefix size to stream: {e}");
+                                continue;
+                            }
+                            if let Err(e) = client.write_all(message_buf) {
+                                eprintln!("Failed to write message to stream: {e}");
+                                continue;
+                            }
+                            if let Err(e) = client.flush() {
+                                eprintln!("Failed to flush the stream buffer: {e}");
+                                continue;
+                            }
                         }
                     }
                 }
             }
         }
-
-        // while let Ok((new_client, id)) = client_rx.try_recv() {
-        //     clients.push((new_client, id));
-        // }
-
-
-        // while let Ok(msg) = msg_rx.try_recv() {
-        //     for (client, id) in clients.iter_mut() {
-        //         if msg.client_id != *id {
-        //             let _ = client.write_all(msg.message.as_bytes()).unwrap();
-        //         }
-        //     }
-        // }
-
-        // thread::sleep(Duration::from_millis(1));
     }
 }
 
-fn handle_clients(mut stream: TcpStream, tx: Sender<ServerMessages>, client_id: String) {
-    // use tx to send the input from user
-
-    loop {
-        let mut buf = [0; 1024];
-        let read_bytes = stream.read(&mut buf).unwrap();
-
-        if read_bytes == 0 {
-            break;
-        }
-
-        //changed having two receiving channels to a single receiver based on feedback provided.
-        let client_message = ServerMessages::ClientMessage(ClientMsg {
-            message: String::from_utf8_lossy(&buf[..read_bytes]).to_string(),
-            client_id: client_id.clone()
-        });
-        tx.send(client_message).unwrap();
-
-        //stream.write(&buf[..read_bytes]).unwrap();
-        // let msg = String::from_utf8_lossy(&buf[..read_bytes]).to_string();
-    }
-}
-
-fn generate_client_id() -> String {
-    let charset = "aABbcCDdeEfFGgHhiIJjkKlLmMNnoOPpqQrRsStTuUvVWwxXyYzZ1!2@3#4$5%6^7&8*9(";
-    let cid = generate(16, charset);
-    cid
-}
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    // Changed to a single channel utilizing an enum.  Based on provided feedback from null_reference_user
-    // on reddit.
-    let (tx, rx) = mpsc::channel();
-    let mut thread_vec: Vec<thread::JoinHandle<_>> = Vec::new();
-
-    let broadcast = thread::spawn(move || {
-        broadcast_message(rx);
-    });
-
-    for stream in listener.incoming() {
-        let client_id = generate_client_id();
-        let stream = stream.unwrap();
-        let tx_clone: Sender<ServerMessages> = tx.clone();
-        tx_clone.send(ServerMessages::Newclient(stream.try_clone().unwrap(), client_id.clone())).unwrap();
-
-        // connection thread handler
-        let tx_clone = tx_clone.clone();
-        let handle = thread::spawn(move || {
-            let _ = handle_clients(stream, tx_clone, client_id);
-
-        });
-        // join handle threads
-        thread_vec.push(handle);
-
-    }
-
-    // for handle in thread_vec {
-    //     handle.join().unwrap();
-    // }
-    // broadcast.join().unwrap();
-    // join broadcast threads
+fn gen_id() -> String {
+    let charset = "AbCdEfGhIjKlMnOpQrStUvWxYz!1@2#3$4%5^6&7*8(9)0";
+    generate(16, charset)
 }
